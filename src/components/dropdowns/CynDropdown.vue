144b9e6896faf900730f41046b8ef443f31f1b23
Alex Petroski <apetroski@cynerge.com>
2019-11-27 06:44:27 -0500

    Dropdown working, more generic. Currently selected still needs fixing

--- src/components/dropdowns/CynDropdown.vue
+++ src/components/dropdowns/CynDropdown.vue
@@ -1,39 +1,26 @@
 <template>
-    <div id="dropdown-div">
-        <label for="users">Select Users:</label>
-        <br />
-        <select v-model="selected">
-            <option value disabled>Please Select a User</option>
-            <option v-bind:value="user.name" v-for="user in users" :key="user.id"
-                >{{ user.name }} | ID: {{ user.id }}</option
-            >
-        </select>
-        <p>Current user selected: {{ selected }}</p>
-    </div>
+  <div id="dropdown-div">
+    <label for="users">Select Users:</label>
+    <br />
+    <select v-model="selected">
+      <option disabled>Please Select a User</option>
+      <option
+        :value="items.name"
+        v-for="item in items"
+        :key="item.id"
+      >{{ item.name }} | ID: {{ item.id }}</option>
+    </select>
+    <p>Current user selected: {{ selected }}</p>
+  </div>
 </template>
 
 <script>
-import axios from 'axios';
-
 export default {
-    name: 'CynDropdown',
-    data() {
-        return {
-            users: null,
-            selected: 'None',
-            defaultSelected: 'Please select a user'
-        };
-    },
-    created() {
-        axios
-            .get('https://jsonplaceholder.typicode.com/users')
-            .then(res => {
-                this.users = res.data;
-            })
-            .catch(err => {
-                this.users = 'An error has occured: ' + err;
-            });
-    }
+  name: "CynDropdown",
+  props: {
+    items: Array,
+    selected: String
+  }
 };
 </script>
 
--- src/views/Dropdowns.vue
+++ src/views/Dropdowns.vue
@@ -1,16 +1,33 @@
 <template>
-    <div>
-        <CynDropdown />
-    </div>
+  <div>
+    <CynDropdown :items="items" :selected="selected" />
+  </div>
 </template>
 
 <script>
-import CynDropdown from '../components/dropdowns/CynDropdown';
+import CynDropdown from "../components/dropdowns/CynDropdown";
+import axios from "axios";
 export default {
-    name: 'dropdowns',
-    components: {
-        CynDropdown
-    }
+  name: "dropdowns",
+  components: {
+    CynDropdown
+  },
+  data() {
+    return {
+      items: [],
+      selected: "None"
+    };
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then(res => {
+        this.items = res.data;
+      })
+      .catch(err => {
+        this.items = "An error has occured: " + err;
+      });
+  }
 };
 </script>
 
